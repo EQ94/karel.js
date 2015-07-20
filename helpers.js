7321c32c80f1c719d1937dfc0588686c271f7244
lambdax-xx <lambdax-xx@users.noreply.github.com>
2015-07-21 04:45:11 +0800

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,31 @@
+// multipart/form-data
+exports.parseMultipart = function (str) {
+		var sp = str.split("\r\n")[0];
+		
+		var kvs = str.split(sp).slice(1, -1);
+
+		var data = {};
+
+		var g = /name="(.*)"/;
+
+		for (var i = 0; i < kvs.length; i++) {
+			var kv = kvs[i];
+			var r = g.exec(kv);
+			if (!r) continue;
+
+			var s = kv.search('\r\n\r\n') + 4;
+			var l = kv.length - s - 2;
+
+			data[r[1]] = kv.substr(s, l);
+		}
+
+		//console.log(str);
+		//console.log(data);
+		
+		return data;
+}
+
+// application/x-www-form-urlencoded
+exports.parseUrlencoded = function (str) {
+	throw new Error('To be implemented!');
+}
